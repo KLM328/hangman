8cc34691c2181771053423b8d4b83a961083c6d1
quentin-cillierre <quentin.cillierre@imt-atlantique.net>
2026-06-24 05:16:55 +0200

    update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,13 +2,11 @@ module fr.quentincillierre.hangman {
     requires javafx.controls;
     requires javafx.fxml;
 
-
-    opens fr.quentincillierre.hangman to javafx.fxml;
-    exports fr.quentincillierre.hangman;
     exports fr.quentincillierre.hangman.application;
     opens fr.quentincillierre.hangman.application to javafx.fxml;
+
     exports fr.quentincillierre.hangman.controller;
     opens fr.quentincillierre.hangman.controller to javafx.fxml;
+
     exports fr.quentincillierre.hangman.model;
-    opens fr.quentincillierre.hangman.model to javafx.fxml;
 }
