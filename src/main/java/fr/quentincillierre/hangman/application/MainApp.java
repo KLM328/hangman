b0a52ac10f5ea4253437804ce4cc67bec9605e8b
quentin-cillierre <quentin.cillierre@imt-atlantique.net>
2026-06-24 05:24:04 +0200

    student-template

--- src/main/java/fr/quentincillierre/hangman/application/MainApp.java
+++ src/main/java/fr/quentincillierre/hangman/application/MainApp.java
@@ -1,37 +1,16 @@
 package fr.quentincillierre.hangman.application;
 
-import fr.quentincillierre.hangman.controller.GameController;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("game-view.fxml"));
 
-        BorderPane root = loader.load();
-
-        GameController controller = loader.getController();
-
-        Scene scene = new Scene(root, 600, 400);
-
-        scene.setOnKeyTyped(event -> {
-            controller.handleKeyboardInput(event.getCharacter());
-        });
-
-        primaryStage.setTitle("HangMan");
-        primaryStage.setScene(scene);
-        primaryStage.setResizable(false);
-
-        primaryStage.show();
     }
 
     public static void main(String[] args) {
-        // Lance l'application JavaFX (qui appellera la méthode start)
         launch(args);
     }
 }
--- src/main/java/fr/quentincillierre/hangman/model/HangmanModel.java
+++ src/main/java/fr/quentincillierre/hangman/model/HangmanModel.java
@@ -1,70 +1,27 @@
 package fr.quentincillierre.hangman.model;
 
-import java.util.LinkedHashSet;
-import java.util.Set;
-
 public class HangmanModel {
-    private final String wordToGuess;
-    private final int maxWrongs;
-    private int currentWrongs;
-    private Set<Character> guessedLetter;
 
-    public HangmanModel(String wordToGuess) {
-        this.wordToGuess = wordToGuess.toUpperCase();
-        this.maxWrongs = 10;
-        this.currentWrongs = 0;
-        this.guessedLetter = new LinkedHashSet<>();
+    public HangmanModel(String java) {
     }
 
-    public Set<Character> getGuessedLetter() {
-        return guessedLetter;
+    public void tryLetter(Character letter) {
+        
     }
 
-    public int getCurrentWrongs() {
-        return currentWrongs;
-    }
-     public String getWordToGuess(){
-        return this.wordToGuess;
-     }
-
-    public void tryLetter(Character letter){
-        letter = Character.toUpperCase(letter);
-        if (this.guessedLetter.contains(letter)){
-            return;
-        }
-        if (!wordToGuess.contains(letter.toString())){
-            currentWrongs++;
-        }
-        guessedLetter.add(letter);
+    public String getHiddenWord() {
+        return "_______";
     }
 
-    public String getHiddenWord(){
-        StringBuilder hiddenWord = new StringBuilder();
-
-        for (int i = 0; i<wordToGuess.length(); i++){
-            Character letter = wordToGuess.charAt(i);
-            if (guessedLetter.contains(letter)){
-                hiddenWord.append(letter);
-            }else {
-                hiddenWord.append('_');
-            }
-        }
-        return hiddenWord.toString().trim();
-    }
-
-    public boolean isWin(){
-        for (Character letter : this.wordToGuess.toCharArray()){
-            if (!this.guessedLetter.contains(letter))
-                return false;
-        }
+    public boolean isWin() {
         return true;
     }
 
-    public boolean isLose(){
-        return currentWrongs >= maxWrongs;
+    public boolean isLose() {
+        return false;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         HangmanModel game = new HangmanModel("java");
         game.tryLetter('R');
         System.out.println(game.getHiddenWord());
@@ -85,16 +42,7 @@ public class HangmanModel {
         System.out.println(game.isLose() ? "LOSE" : "KEEP PUSHING");
         game.tryLetter('g');
         System.out.println(game.isLose() ? "LOSE" : "KEEP PUSHING");
-
-        System.out.println(game.getGuessedLetter());
-
-        System.out.println();
-
-
-
-
     }
-
 
 
 }
